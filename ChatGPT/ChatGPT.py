7ef3b51c5e4ee9baaa76fc5fbeec048fe7ec653c
DanielNadrchal <danieln505.cs@gmail.com>
2023-08-21 18:38:36 -0500

    Print response time. Save chatLogs

--- ChatGPT/ChatGPT.py
+++ ChatGPT/ChatGPT.py
@@ -2,14 +2,30 @@ import os
 import openai
 import requests
 import json
+import time
+import secrets
+
+
 
 def main():
-    modelTarget = "gpt-3.5-turbo"
+    modelTarget = "gpt-4"
+    #modelTarget = "gpt-3.5-turbo"
     openai.api_key = os.getenv("OPENAI_API_KEY")
     print("Python API REPL for " + modelTarget)
     print("Enter 'exit' to quit")
     allMessages = []
     
+    filename = secrets.token_hex(6)
+    filename = 'chatLogs\\' + filename + '.txt'   
+
+    try:
+        os.makedirs('chatLogs')
+    except OSError:
+        print ("Creation of the chatLogs directory failed")
+    else:
+        print ("Successfully created the chaLogs directory")    
+
+
     while True:
         user_input = input("Enter your input: ")
 
@@ -17,6 +33,7 @@ def main():
             print("Exiting...")
             break
         
+        start_time = time.time()
         moderationResults = openai.Moderation.create(user_input)
         
         messageIsFlagged = moderationResults.results[0].flagged
@@ -36,7 +53,13 @@ def main():
         
         allMessages.append(apiMessageResponse)
         
+        end_time = time.time()
+        print("\nTime taken: ", end_time - start_time, " seconds.\n\n")
         print(apiMessageResponse.content)
+        
+        with open(filename, 'w') as f:
+            for item in allMessages:
+                f.write("%s\n" % item)
         
 
 if __name__ == "__main__":
